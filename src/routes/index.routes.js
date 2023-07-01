6a5aacd968136a5be2a32bcc1a81e6e5e97e36ba
CodeHxH <rodriguezjoseagustin31@gmail.com>
2023-06-30 21:09:55 -0400

    fix second commit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 const { renderIndex } = require('../handlers/handlers');
-const { validateForm } = require('../helpers/helpers');
 
 router.get('/', renderIndex);
 
